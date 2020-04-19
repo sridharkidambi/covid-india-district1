5efb3a6d9febc4a7846f0502b3befd4fb67af99b
sridhar.kidambi@wipro.com <sridhar.kidambi@wipro.com>
2020-04-19 14:06:11 +0530

    covid india

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('banking-app app is running!');
+    expect(page.getTitleText()).toEqual('covid-india-district app is running!');
   });
 
   afterEach(async () => {
